6d00d2a6ce369eb3fd88b2b0277d68d2eb242f86
noneAuth <wb-zqm915773@antgroup.com>
2023-11-03 17:51:37 +0800

    feat: update editor

--- src/components/LanguageQuery/LanguageEditor/index.tsx
+++ src/components/LanguageQuery/LanguageEditor/index.tsx
@@ -1,5 +1,5 @@
-import React, { forwardRef } from 'react';
 import CypherEdit from './cypherEditor';
+import React, { forwardRef } from 'react';
 
 interface Props {
   language?: 'ISOGQL' | 'Gremlin';
@@ -12,27 +12,30 @@ interface Props {
   gremlinId?: string;
 }
 
-export const GraphEditor: React.FC<Props, any> = forwardRef<any, any>(
-  (props, editorRef) => {
-    const {
-      height = '100%',
-      width,
-      initialValue = '',
-      graphId = 'default-editor',
-      language,
-    } = props;
-    return (
-      <div style={{ width, height, borderRadius: 8 }}>
-        <CypherEdit
-          ref={editorRef}
-          value={initialValue}
-          height={height}
-          showGutter={false}
-          language={language}
-        />
-      </div>
-    );
-  }
-);
+export const GraphEditor: React.FC<Props> = forwardRef((props) => {
+  const {
+    height = '100%',
+    width,
+    onChange,
+    initialValue = '',
+    graphId = 'default-editor',
+  } = props;
+
+  return (
+    <div style={{ width, height, borderRadius: 8 }}>
+      <CypherEdit
+        gremlinId={graphId}
+        initValue={initialValue}
+        height={height}
+        showGutter={false}
+        onValueChange={(val) => {
+          if (onChange) {
+            onChange(val);
+          }
+        }}
+      />
+    </div>
+  );
+});
 
 export default GraphEditor;
